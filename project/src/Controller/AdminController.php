fb9ba8ead1e903718c9f27b249119cbf7708be20
Gl3diator <ghaith.bensalah@esprit.tn>
2026-04-05 06:13:20 +0100

    feat(serinity-web): add backoffice structure and admin routes

--- project/src/Controller/AdminController.php
+++ project/src/Controller/AdminController.php
@@ -6,13 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/admin')]
 final class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
+    #[Route('', name: 'app_admin_dashboard', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        return $this->render('admin/dashboard.html.twig');
     }
 }
