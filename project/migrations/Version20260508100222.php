a78e47ad1f83d51687f492a26b241f861bc11cd5
Gl3diator <ghaith.bensalah@esprit.tn>
2026-05-08 11:37:28 +0100

    refactor(conf): Setup project packages

--- project/migrations/Version20260508100222.php
+++ project/migrations/Version20260508100222.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260504174509 extends AbstractMigration
+final class Version20260508100222 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20260504174509 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users DROP risk_evaluated_at');
+        $this->addSql('ALTER TABLE exercice ADD benefits LONGTEXT DEFAULT NULL, ADD tips LONGTEXT DEFAULT NULL, ADD theme VARCHAR(50) DEFAULT NULL, ADD guided_instructions JSON DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users ADD risk_evaluated_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE exercice DROP benefits, DROP tips, DROP theme, DROP guided_instructions');
     }
 }
